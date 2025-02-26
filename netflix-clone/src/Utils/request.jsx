c0444e32311c65cbd9369e7bebb7ed4f6529546a
NatinaelGebeyehu <natinaelgebeyehu2127@gmail.com>
2025-02-26 14:48:27 +0300

    4

--- netflix-clone/src/Utils/request.jsx
+++ netflix-clone/src/Utils/request.jsx
@@ -1,4 +1,4 @@
-const API_KEY="efa64fc1de1a5edae30e79ff06a621db"
+const API_KEY=process.env.REACT_APP_API_KEY;
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
